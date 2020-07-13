5059e837f01ce851a0d7b99d34f464bc95820c40
yxj0312 <yanxiajun@hotmail.com>
2020-07-13 22:17:17 +0200

    Move construtor to game

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-      xIsNext: true,
+      NNext: true,
     };
   }
 
@@ -73,6 +73,15 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext:true,
+    }
+  }
   render() {
     return (
       <div className="game">
