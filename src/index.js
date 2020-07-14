c62d84cb58f7d9ae511d6333ecf9bbd402c3f1a4
yxj0312 <yanxiajun@hotmail.com>
2020-07-14 16:35:54 +0200

    Showing the past moves

--- src/index.js
+++ src/index.js
@@ -69,11 +69,23 @@ class Game extends React.Component {
       xIsNext: !this.state.xIsNext,
     });
   }
-  
+
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? 
+        'Go to move # ' + move: 
+        'Go to game start';
+        return (
+          <li>
+            <button onClick={()=>this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+    });
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -90,7 +102,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
