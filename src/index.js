fe52d7ac479ec1a2cc3058e98618bcbeaf8a8715
yxj0312 <yanxiajun@hotmail.com>
2020-07-17 17:20:25 +0200

    task2: Bold the currently selected item in the move list.

--- src/index.js
+++ src/index.js
@@ -103,7 +103,8 @@ class Game extends React.Component {
         'Go to game start';
         return (
           <li key={move}>
-            <button onClick={()=>this.jumpTo(move)}>{desc}</button>
+            <button onClick={()=>this.jumpTo(move)}>
+            {move === this.state.stepNumber ? <b>{desc}</b> : desc}</button>
           </li>
         );
     });
